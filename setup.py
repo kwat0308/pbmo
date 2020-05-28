e7b201371324bd3dacd660a23a0bae5294648134
Keito Watanabe <keito@ualberta.ca>
2020-05-28 15:57:29 -0600

    Fixed program for command line installation

--- setup.py
+++ setup.py
@@ -8,40 +8,48 @@ from setuptools import setup, Extension, find_packages
 local_path = os.path.dirname(os.path.abspath(__file__))
 # change compiler
 # os.environ["CC"] = "clang++"
-os.environ["CC"] = "g++"
+# os.environ["CC"] = "g++"
+os.environ["CC"] = "cl"
+
+
+# obtained from: https://github.com/pybind/python_example/blob/master/setup.py
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked. """
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include()
 
 ext_module = [
     Extension(
-    'funcs',
-    sources=["cpptasks/src/funcs.cc", "cpptasks/src/funcs_wrapper.cc"],
-    depends=[],
-    language='c++',
-    # extra_compile_args=[    # calls error when run by standard C++ compiler (Windows)
-    #     '-std=c++14',  
-    #     '-Wno-unused-function',
-    #     '-Wno-write-strings',
-    # ],
-),
+        'funcs',
+        sources=["cpptasks/src/funcs.cc", "cpptasks/src/funcs_wrapper.cc"],
+        depends=[],
+        language='c++',
+        include_dirs=[
+            get_pybind_include(),
+        ]
+    ),
     Extension(
         'Matrix',
         sources=["cpptasks/src/Matrix.cc", "cpptasks/src/Matrix_wrapper.cc"],
         depends=[],
         language='c++',
         include_dirs=[
+            get_pybind_include(),
             'cpptasks/include'
         ],
-        # extra_compile_args=[
-        #     '-std=c++14',  
-        #     '-Wno-unused-function',
-        #     '-Wno-write-strings',
-        # ],
     )
 ]
 
 setup(
     name='myModule',
     version=1.0,
-    description='Example module that contains some basic functions and a matrix class.',
+    description=
+    'Example module that contains some basic functions and a matrix class.',
     packages=find_packages(exclude='tests'),
     ext_modules=ext_module,
     classifiers=[
@@ -59,6 +67,13 @@ setup(
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Natural Language :: English',
-        
     ],
 )
+
+
+
+# extra_compile_args=[    # calls error when run by standard C++ compiler (Windows)
+#     '-std=c++14',   # require C++14 or higher
+#     '-Wno-unused-function',  # do not raise error even when function is unused
+#     '-Wno-write-strings',   
+# ],
