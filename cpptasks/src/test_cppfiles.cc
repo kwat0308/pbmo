7db21cf92188066c3b74b07ca45849dbbf6388b6
Keito Watanabe <keito@ualberta.ca>
2020-05-28 16:00:33 -0600

    Fixed path to data file

--- cpptasks/src/test_cppfiles.cc
+++ cpptasks/src/test_cppfiles.cc
@@ -2,14 +2,14 @@
 #include "Matrix.h"
 #include <iostream>
 
-int main() 
+int main()
 {
 
     std::cout << "Hello\n";
     // double data[12] {1,2,3,4,5,6,7,8,9,0,1,2};
     // Matrix M {3,4, data};
     // M.print();
-    Matrix M {3000, 4000, "../../data/large_data.txt"};
+    Matrix M{3000, 4000, "../data/large_data.txt"};
     // M.print();
     // std::cout << M.rowsize();
 
