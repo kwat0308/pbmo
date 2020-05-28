489b50d53e67d042cf9386c7c574afce558ce209
Keito <k.wat8973@gmail.com>
2020-05-28 11:39:36 -0600

    Fixed const declaration for private members

--- cpptasks/include/Matrix.h
+++ cpptasks/include/Matrix.h
@@ -2,7 +2,6 @@
 #ifndef __MATRIX_H_
 #define __MATRIX_H_
 
-
 #include <vector>
 #include <string>
 #include <iostream>
@@ -12,7 +11,6 @@
 #include <ios>
 #include <numeric>
 // #include <Eigen/Dense>
-
 
 /* This code should have a Matrix class that has standard operations using vector<vector<T> > as our matrix.
 Our custom created Matrix class should: 
@@ -41,46 +39,46 @@ With this, we can compare our Matrix class to:
 
 */
 
-class Matrix {
-    private:
-        //members
-        int rsz;  //row size
-        int csz;  //column size
-        double* m;   // pointer to 2-d array
-    
-    public:
-        //constructor
-        Matrix(const int&, const int&);
-        Matrix(const int&, const int&, double data[]);
-        Matrix(const int&, const int&, const std::string&);
-        // copy 
-        Matrix(const Matrix&);   // copy constructor
-        Matrix& operator=(const Matrix&);  //copy assignment
+class Matrix
+{
+private:
+    //members
+    const int rsz;   //row size
+    const int csz;   //column size
+    const double *m; // pointer to 2-d array
 
-        // move
-        Matrix(Matrix&&);   // move constructor
-        Matrix& operator=(Matrix&&);   // move assignment
+public:
+    //constructor
+    Matrix(const int, const int);
+    Matrix(const int, const int, double data[]);
+    Matrix(const int, const int, const std::string &);
+    // copy
+    Matrix(const Matrix &);            // copy constructor
+    Matrix &operator=(const Matrix &); //copy assignment
 
-        // setters
-        void set_value(const int& i, const int& j, const double& newval) {m[i*csz+j] = newval;}  // change single value
+    // move
+    Matrix(Matrix &&);            // move constructor
+    Matrix &operator=(Matrix &&); // move assignment
 
-        // getters
-        const int rowsize() const {return rsz;}   // number of rows
-        const int columnsize() const {return csz;}    // number of columns
-        const std::vector<int> dim() const {return std::vector<int> {rsz, csz};}   // dimension
-        const double get_value(const int& i, const int& j) const {return m[i*csz + j];} // value at (i,j) coordinate
+    // setters
+    void set_value(const int i, const int j, const double &newval) { m[i * csz + j] = newval; } // change single value
 
-        // operations
-        bool dim_equal(const Matrix&);   // check if dimensions are equal
-        double inner_prod(const Matrix&);
-        double norm();  //Frobenius norm (standard matrix norm)
-        // utility functions
-        void print_mat();   // print matrix
-        void print_row(const int&);   // auxiliary function for print_mat() to print each row
-        // Matrix import_data(const int&, const int&, const std::string&);  // import data (knowing data dimensions)
-        // destructor
-        ~Matrix() {delete[] m;}
+    // getters
+    const int rowsize() const { return rsz; }                                         // number of rows
+    const int columnsize() const { return csz; }                                      // number of columns
+    const std::pair<int, int> dim() const { return std::pair<int, int>{rsz, csz}; }   // dimension
+    const double get_value(const int i, const int j) const { return m[i * csz + j]; } // value at (i,j) coordinate
 
+    // operations
+    bool dim_equal(const Matrix &); // check if dimensions are equal
+    double inner_prod(const Matrix &);
+    double norm(); //Frobenius norm (standard matrix norm)
+    // utility functions
+    void print_mat();            // print matrix
+    void print_row(const int &); // auxiliary function for print_mat() to print each row
+    // Matrix import_data(const int&, const int&, const std::string&);  // import data (knowing data dimensions)
+    // destructor
+    ~Matrix() { delete[] m; }
 };
 
 #endif
